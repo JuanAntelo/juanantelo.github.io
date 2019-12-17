54be142f756da5378f6dea9da2c36fe98a72a10f
Juan Antelo - i867899 <juan.antelo@sap.com>
2019-12-17 10:02:34 -0500

    .

---
+++ precache-manifest.c5cf14fc65dec5f27e0d226e0c730b08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d55bf15e3b0df7082cdb640f23c519",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db543bc3659c01a004ff",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "08a209d6c88402208064",
+    "url": "/static/js/2.723bdfe2.chunk.js"
+  },
+  {
+    "revision": "db543bc3659c01a004ff",
+    "url": "/static/js/main.129af075.chunk.js"
+  },
+  {
+    "revision": "d653c3e1ff0c4fc20192",
+    "url": "/static/js/runtime-main.d01d61ca.js"
+  }
+]);
